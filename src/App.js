0266d9417b61ebf8a818fdd888cecc9b8f90f491
James Hagood <cardfaux@tutanota.com>
2020-03-03 13:04:16 -0600

    added more packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const worker = createWorker({
+		logger: (m) => console.log(m)
+	});
+	const doOCR = async () => {
+		await worker.load();
+		await worker.loadLanguage('eng');
+		await worker.initialize('eng');
+		const {
+			data: { text }
+		} = await worker.recognize(
+			'https://tesseract.projectnaptha.com/img/eng_bw.png'
+		);
+		setOcr(text);
+	};
+	const [ocr, setOcr] = useState('Recognizing...');
+	useEffect(() => {
+		doOCR();
+	});
+	return (
+		<div className='App'>
+			<p>{ocr}</p>
+		</div>
+	);
 }
 
 export default App;
