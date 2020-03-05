b19efd72862f8b0d0ad05ad7bd66167a214caacd
James Hagood <cardfaux@tutanota.com>
2020-03-05 07:36:00 -0600

    changed Buyer Card

--- src/components/HomePage/BuyerCard/BuyerCard.jsx
+++ src/components/HomePage/BuyerCard/BuyerCard.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Card from '../../../shared/UIElements/Card';
 
+import { Melon } from '../../../styles/Colors';
+
 const BuyerCard = (props) => {
 	return (
 		<div className={props.className}>
-			<Card>
-				<h1>Buyer Card</h1>
-			</Card>
+			<Link to='/buyer'>
+				<Card>
+					<h1>Buyer Card</h1>
+				</Card>
+			</Link>
 		</div>
 	);
 };
 
 export default styled(BuyerCard)`
 	margin: 4rem;
+	> a {
+		text-decoration: none;
+		color: black;
+		&:hover {
+			color: ${Melon};
+		}
+	}
 `;
